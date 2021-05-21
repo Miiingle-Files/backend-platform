7e09b661696b399e072aa29b0019a3c554b9c894
LM Bibera <lyndon.bibera@headhuntr.io>
2021-05-21 21:49:44 +0800

    add a POC for using some AWS service via sdk

--- src/main/java/net/miiingle/files/platform/BookService.java
+++ src/main/java/net/miiingle/files/platform/BookService.java
@@ -1,18 +1,31 @@
 package net.miiingle.files.platform;
 
 import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.Bucket;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Slf4j
 @Singleton
 public class BookService {
 
+    @Inject S3Client s3;
+
     public BookService() {
         log.info("Initialized BookService");
+
+       //TODO: connect to aurora via data api
     }
 
     public void doSomething() {
         log.info("Bookservice did something!");
+
+        var buckets = s3.listBuckets();
+
+        buckets.buckets().stream()
+                .map(Bucket::name)
+                .forEach(log::info);
     }
 }
